2dcdacd2522ce0f0e01cbc81e5143a6b1b7ca53c
gabap <khiempham201@gmail.com>
2023-12-20 16:04:43 +0700

    f

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -49,38 +49,5 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-export default {
-  data: () => ({
-    user: {
-      username: null,
-      password: null
-    }
-  }),
-  computed: {
-    ...mapGetters({
-      authUser: 'auth/user'
-    })
-  },
-  methods: {
-    ...mapActions({
-      loginUser: 'auth/loginUser'
-    }),
-    async login() {
-      await this.loginUser(this.user)
-        .then(() => {
-          if (this.authUser.authenticated) {
-            this.$router.push('/secure')
-          } else {
-            // Handle error
-            this.user = {
-              username: null,
-              password: null
-            }
-          }
-        })
 
-    }
-  }
-}
 </script>
